c6fe7c03bc94e5a5e701fe5fa129888389d67685
Ryan Quigley <rquig14@gmail.com>
2017-04-02 17:00:06 -0700

    Added roxygen2 documentation to clustercheck.R; function help can now be accessed with ?clustercheck

--- R/clustercheck.R
+++ R/clustercheck.R
@@ -1,7 +1,30 @@
-
-
-# in the results table, true clusters are the rows, guessed clusters are the columns
-
+#'Check accuracy of cluster assignments
+#'
+#'Calculate accuracy of the clustering algorithm by comparing algorithm
+#'generated cluster labels to the ground truth cluster labels.
+#'
+#'In the results matrix, ground truh cluster labels are rows and the algorithm
+#'generated cluster labels are columns.
+#'@param cluster Integer vector of algorithm generated cluster labels.
+#'@param Labels Integer vector of ground truth cluster labels.
+#'@param k Integer indicating the number of clusters.
+#'
+#'@return List with two elements:
+#'  \enumerate{
+#'    \item Matrix of counts indicating how the ground truth labels are
+#'    distributed across the algorithm generated cluster labels.
+#'    \item Numeric. Percentage correctly assigned.
+#'  }
+#'
+#' @examples
+#' data(cluster6)
+#' # Perfect cluster assignment
+#' clustercheck(cluster6_labels, cluster6_labels, 6)
+#'
+#' # Random cluster assignment
+#' n <- length(cluster6_labels)
+#' random_labels <- sample(1:6, size = n, replace = TRUE)
+#' clustercheck(random_labels, cluster6_labels, 6)
 clustercheck <- function (cluster, Labels, k) {
 
 
